c5fdaeb0ea7fee202f94b4c1c42481f1b5241db2
Truong Duc Thang <ducthangm9@gmail.com>
2024-06-04 22:49:30 +0700

    init project v5

---
+++ src/app/auth.ts
@@ -0,0 +1,47 @@
+import NextAuth from 'next-auth';
+import Cognito from 'next-auth/providers/cognito';
+import GitHub from 'next-auth/providers/github';
+import type { Provider } from "next-auth/providers"
+
+const providers: Provider[] =  [Cognito, GitHub]
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+  providers: providers,
+  basePath: '/auth',
+  pages: {
+    signIn: "/signin",
+  },
+  // callbacks: {
+  //   authorized({ request, auth }) {
+  //     const { pathname } = request.nextUrl
+  //     if (pathname === "/middleware-example") return !!auth
+  //     return true
+  //   },
+  //   jwt({ token, trigger, session, account }) {
+  //     if (trigger === "update") token.name = session.user.name
+  //     if (account?.provider === "keycloak") {
+  //       return { ...token, accessToken: account.access_token }
+  //     }
+  //     return token
+  //   },
+  //   async session({ session, token }) {
+  //     if (token?.accessToken) {
+  //       session.accessToken = token.accessToken
+  //     }
+  //     return session
+  //   },
+  // },
+  // experimental: {
+  //   enableWebAuthn: true,
+  // },
+  // debug: process.env.NODE_ENV !== 'production' ? true : false,
+});
+
+export const providerMap = providers.map((provider) => {
+  if (typeof provider === "function") {
+    const providerData = provider()
+    return { id: providerData.id, name: providerData.name }
+  } else {
+    return { id: provider.id, name: provider.name }
+  }
+})
--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,8 +1,8 @@
-'use client';
 
 import React from 'react';
 import { Button, FormProps, Typography, Checkbox, Form, Input } from 'antd';
-import { signIn } from 'next-auth/react';
+import { signIn, auth, providerMap } from "@/app/auth"
+import { AuthError } from "next-auth"
 
 type Props = {};
 
@@ -31,9 +31,10 @@ const onFinishFailed: FormProps<FieldType>['onFinishFailed'] = (errorInfo) => {
 };
 
 export default function LoginPage({}: Props) {
+    console.log(11,process.env.AUTH_GITHUB_ID)
   return (
     <div className="flex items-center justify-center min-h-screen">
-      <Form
+      {/* <Form
         name="basic"
         style={{ maxWidth: 600 }}
         initialValues={{ remember: true }}
@@ -63,7 +64,37 @@ export default function LoginPage({}: Props) {
             Submit
           </Button>
         </Form.Item>
-      </Form>
+      </Form> */}
+
+      <div className="flex flex-col gap-2">
+      {Object.values(providerMap).map((provider) => (
+        <form
+          action={async () => {
+            "use server"
+            try {
+              await signIn(provider.id)
+            } catch (error) {
+              // Signin can fail for a number of reasons, such as the user
+              // not existing, or the user not having the correct role.
+              // In some cases, you may want to redirect to a custom error
+              if (error instanceof AuthError) {
+                // return redirect(`${SIGNIN_ERROR_URL}?error=${error.type}`)
+              }
+ 
+              // Otherwise if a redirects happens NextJS can handle it
+              // so you can just re-thrown the error and let NextJS handle it.
+              // Docs:
+              // https://nextjs.org/docs/app/api-reference/functions/redirect#server-component
+              throw error
+            }
+          }}
+        >
+          <button type="submit">
+            <span>Sign in with {provider.name}</span>
+          </button>
+        </form>
+      ))}
+    </div>
     </div>
   );
 }
