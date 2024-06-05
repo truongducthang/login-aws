3f04a465efc393d6db894078f767f901256169dd
Truong Duc Thang <ducthangm9@gmail.com>
2024-06-05 22:13:38 +0700

    add signout

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -34,6 +34,22 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   //     return session
   //   },
   // },
+
+  // callbacks: {
+  //   async signIn({ user, account, profile, email, credentials }) {
+  //     return true
+  //   },
+  //   async redirect({ url, baseUrl }) {
+  //     return baseUrl
+  //   },
+  //   async session({ session, token, user }) {
+  //     return session
+  //   },
+  //   async jwt({ token, user, account, profile, isNewUser }) {
+  //     return token
+  //   }
+  // }
+  
   // experimental: {
   //   enableWebAuthn: true,
   // },
