464a4896ad670339aba914d27222c42db8987b32
Truong Duc Thang <ducthangm9@gmail.com>
2024-06-09 13:38:57 +0700

    remote url

--- next.config.mjs
+++ next.config.mjs
@@ -5,11 +5,11 @@ const nextConfig = {
         AWS_SECRET_ACCESS_KEY: 'AWS_SECRET_ACCESS_KEY',
         COGNITO_REGION: 'COGNITO_REGION',
         
-        AUTH_GITHUB_ID: 'Ov23li8hMFB1hxf3MSlG',
-        AUTH_GITHUB_SECRET: '4bd10c5ec17c3733949fd90c2d91b2b770c93d80',
+        AUTH_GITHUB_ID: '',
+        AUTH_GITHUB_SECRET: '',
 
         NEXT_AUTH_SECRET:'NEXT_AUTH_SECRET',
-        AUTH_SECRET: 'g3LjIzGNyWOAMWC+dLGcOAaV7p5zsc2a7WyWT1ttlkE=',
+        AUTH_SECRET: '',
         NEXTAUTH_URL: 'https://login-aws-tdt-1.vercel.app'
     },
     trailingSlash: false
